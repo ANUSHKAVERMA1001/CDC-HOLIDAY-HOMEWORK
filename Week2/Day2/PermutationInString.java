683b94c51f181b9a4ee4277a1bf675131a4e3b93
ANUSHKAVERMA1001 <24110CN277@glbitm.ac.in>
2026-06-18 02:04:46 +0530

    Create PermutationInString.java

---
+++ Week2/Day2/PermutationInString.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s2.length()<s1.length())
+        {
+            return false;
+        }
+       
+        int[] freq1=new int[26];
+        int[] freq2=new int[26];
+        int ls1=s1.length();
+
+        for(int i=0;i<ls1;i++)
+        {
+            freq1[s1.charAt(i)-'a']++;
+            freq2[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(freq1,freq2))
+        {
+            return true;
+        }
+
+        for(int i=ls1;i<s2.length();i++)
+        {
+            freq2[s2.charAt(i)-'a']++;
+            freq2[s2.charAt(i-ls1)-'a']--;
+
+            if(Arrays.equals(freq1,freq2))
+           {
+            return true;
+           }
+        }
+        return false;     
+    }
+}
